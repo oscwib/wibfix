4a15ebf0d700cf7e4df9b7a3cf4e8541136fbd6e
oscwib <33075932+oscwib@users.noreply.github.com>
2018-01-03 08:08:12 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.4.17"
-  LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
+  UA = "Line/7.14.0 iPAD5,1 10.2.0"
+  LA = "IOSIPAD\t7.14.0\tiPhone_OS\t10.12.0"
 
   rev = 0
 
